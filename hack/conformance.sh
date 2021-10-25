44f559175a4a70f0c883bc2adee25739eefcfbad
Or Mergi <ormergi@redhat.com>
2021-10-25 18:56:01 +0300

    conformance: Print test suite to stdout
    This will improve debugging process of failing tests
    when using Kubevirt Conformance plugin.
    Also save the test suite output at artifacts dir.
    
    Signed-off-by: Or Mergi <ormergi@redhat.com>

--- hack/conformance.sh
+++ hack/conformance.sh
@@ -4,6 +4,22 @@
 
 set -eExuo pipefail
 
+kubectl="./cluster-up/kubectl.sh"
+
+function wait_for_sonobuoy_job_to_start() {
+  local -r sonobuoy_job_label="sonobuoy-plugin=kubevirt-conformance"
+  local -r ns="sonobuoy"
+  local pod=""
+  ! timeout 1m bash -ex -c "until ${kubectl} get po -n ${ns} -l ${sonobuoy_job_label} --no-headers | grep .; do sleep 5; done" && \
+    echo "FATAL: sonobouy job did not start on time" >&2 && return 1
+  ${kubectl} wait --timeout="10m" --for="condition=Ready" -n ${ns} -l ${sonobuoy_job_label} pods
+}
+
+function follow_sonobouy_job_logs() {
+  local -r sonobuoy_job_pod=$(${kubectl} get po -n sonobuoy -l sonobuoy-plugin=kubevirt-conformance --no-headers  | awk '{print $1}')
+  ${kubectl} logs -n sonobuoy "${sonobuoy_job_pod}" -c plugin -f
+}
+
 echo 'Preparing directory for artifacts'
 export ARTIFACTS=_out/artifacts/conformance
 mkdir -p ${ARTIFACTS}
@@ -30,7 +46,15 @@ if [[ ! -z "$SKIP_OUTSIDE_CONN_TESTS" ]]; then
 fi
 
 echo 'Executing conformance tests and wait for them to finish'
-sonobuoy run ${sonobuoy_args}
+sonobuoy run ${sonobuoy_args} &
+sonobuoy_exec_pid="$!"
+
+wait_for_sonobuoy_job_to_start
+follow_sonobouy_job_logs 2>&1 | tee "${ARTIFACTS}/kubevirt_tests_suite.log" &
+sonobuoy_job_log_pid="$!"
+
+wait $sonobuoy_exec_pid
+wait $sonobuoy_job_log_pid
 
 trap "{ echo 'Cleaning up after the test execution'; sonobuoy delete --wait; }" EXIT SIGINT SIGTERM SIGQUIT
 
